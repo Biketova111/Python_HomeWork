fda97d831ab920444dcc0f8bb3d18a4a3c726c88
Biketova Elena <89241781930@mail.ru>
2024-07-19 07:27:26 +0900

    HomeWork_3

--- HomeWork_3/task16.py
+++ HomeWork_3/task16.py
@@ -7,20 +7,17 @@ import random
 
 size = int(input("Введите количество элементов в списке: "))
 list_1=[random.randint(0,100) for _ in range(size)]
-print(list_1)
 
 num=int(input("Введите искоиое число: "))
-count=0
-for i in range(size):
-    if list_1[i]==num:
-        count+=1
+count=list_1.count(num)
 
-
-print(f"Число {num} встречается в списке {count} раз")
 
 if count==0:
     blizkoe=list_1[0]
     for i in range(size):
         if abs(blizkoe-num)>abs(list_1[i]-num):
             blizkoe=list_1[i]
+
+print(list_1)
+print(f"Число {num} встречается в списке {count} раз")
 print(f"Ближайшее значение всписке {blizkoe}")
